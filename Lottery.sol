e000a0e8310493656eafd4b9618ddbd98f2fdccc
Marzhan Nurbakyt <55990842+marzhannurbakyt@users.noreply.github.com>
2021-07-19 19:51:03 +0600

    Add files via upload

---
+++ Lottery.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.5.16;
+
+contract Lottery {
+
+    address public manager;
+    address payable[] public players;
+
+    constructor() public {
+        manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > 1 ether);
+
+        players.push(msg.sender);
+    }
+
+    function random() private view returns (uint) {
+        return uint(keccak256(abi.encodePacked(now, msg.sender, this)));
+    }
+
+    function pickWinner() public payable restricted {
+        uint index = random() % players.length;
+        players[index].transfer(address(this).balance);
+        players = new address payable[](0);
+    }
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    function getPlayers() public view returns (address payable[] memory) {
+        return players;
+    }
+    
+    function resetLottery() internal {
+        players = new address payable[](0);
+    }
+
+} 
